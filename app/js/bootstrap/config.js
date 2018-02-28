e92e4e5626f8f00b1e640489ae34cc9056a19fd9
Nate Jensen <najens@gmail.com>
2018-02-28 11:39:15 -0700

    docs: Add bootstrap config for js collapse

--- app/js/bootstrap/config.js
+++ app/js/bootstrap/config.js
@@ -2,7 +2,7 @@
 // import 'exports-loader?Alert!bootstrap/js/dist/alert';
 // import 'exports-loader?Button!bootstrap/js/dist/button';
 // import 'exports-loader?Carousel!bootstrap/js/dist/carousel';
-// import 'exports-loader?Collapse!bootstrap/js/dist/collapse';
+import 'exports-loader?Collapse!bootstrap/js/dist/collapse';
 // import 'exports-loader?Dropdown!bootstrap/js/dist/dropdown';
 // import 'exports-loader?Modal!bootstrap/js/dist/modal';
 // import 'exports-loader?Popover!bootstrap/js/dist/popover';
